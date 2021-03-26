30f4e73f479b062c73faca7c6626a7341d2f1e68
Jowl12 <joegope@epsa.upv.es>
2021-03-26 19:02:12 +0100

    Clase 6

---
+++ Assets/Clase 6/ControlAnimaciones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ControlAnimaciones : MonoBehaviour
+{
+    public Animator animator;
+    public string parametroBoolean;
+    bool b = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        b = !b;
+        animator.SetBool(parametroBoolean, b);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) animator.SetTrigger("destruir");
+    }
+}
